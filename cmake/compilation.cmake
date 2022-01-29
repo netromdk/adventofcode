0200d3f69cd0298ffb6a5c15635e35f8dc95a28f
Morten Kristensen <me@mortens.dev>
2022-01-29 12:06:39 +0100

    Ccache option

--- cmake/compilation.cmake
+++ cmake/compilation.cmake
@@ -1,2 +1,12 @@
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+# Detect if ccache is installed and use if it is the case.
+if (USE_CCACHE)
+  find_program(CCACHE ccache)
+  if (CCACHE)
+    # Specify to launch ccache for compilation and linking globally.
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+  endif()
+endif()
