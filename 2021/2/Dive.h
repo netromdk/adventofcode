d46fc70cfd512c4bf3a01c180cf2f96e0f613fae
Morten Kristensen <me@mortens.dev>
2022-01-30 11:10:26 +0100

    Improved 2021 day 2

--- 2021/2/Dive.h
+++ 2021/2/Dive.h
@@ -7,50 +7,95 @@
 
 namespace Dive {
 
+class CommandProcessor {
+public:
+  int process(const std::vector<std::string> &lines)
+  {
+    for (const auto &line : lines) {
+      std::stringstream ss(line);
+      std::string cmd;
+      int mod = 0;
+      ss >> cmd >> mod;
+      if (cmd == "forward") {
+        forward(mod);
+      }
+      else if (cmd == "down") {
+        down(mod);
+      }
+      else if (cmd == "up") {
+        up(mod);
+      }
+    }
+    return result();
+  }
+
+protected:
+  virtual void forward(int x) = 0;
+  virtual void down(int x) = 0;
+  virtual void up(int x) = 0;
+  virtual int result() const = 0;
+};
+
 int horizontalPosDepth(const std::vector<std::string> &lines)
 {
-  int pos = 0;
-  int depth = 0;
-  for (const auto &line : lines) {
-    std::stringstream ss(line);
-    std::string cmd;
-    int mod = 0;
-    ss >> cmd >> mod;
-    if (cmd == "forward") {
-      pos += mod;
+  class Proc : public CommandProcessor {
+  protected:
+    void forward(int x) override
+    {
+      pos += x;
     }
-    else if (cmd == "down") {
-      depth += mod;
+
+    void down(int x) override
+    {
+      depth += x;
     }
-    else if (cmd == "up") {
-      depth -= mod;
+
+    void up(int x) override
+    {
+      depth -= x;
     }
-  }
-  return pos * depth;
+
+    int result() const override
+    {
+      return pos * depth;
+    }
+
+    int pos = 0;
+    int depth = 0;
+  };
+  return Proc().process(lines);
 }
 
 int horizontalPosDepthAim(const std::vector<std::string> &lines)
 {
-  int pos = 0;
-  int depth = 0;
-  int aim = 0;
-  for (const auto &line : lines) {
-    std::stringstream ss(line);
-    std::string cmd;
-    int mod = 0;
-    ss >> cmd >> mod;
-    if (cmd == "forward") {
-      pos += mod;
-      depth += aim * mod;
+  class Proc : public CommandProcessor {
+  protected:
+    void forward(int x) override
+    {
+      pos += x;
+      depth += aim * x;
     }
-    else if (cmd == "down") {
-      aim += mod;
+
+    void down(int x) override
+    {
+      aim += x;
     }
-    else if (cmd == "up") {
-      aim -= mod;
+
+    void up(int x) override
+    {
+      aim -= x;
     }
-  }
-  return pos * depth;
+
+    int result() const override
+    {
+      return pos * depth;
+    }
+
+    int pos = 0;
+    int depth = 0;
+    int aim = 0;
+  };
+  return Proc().process(lines);
 }
 
 } // namespace Dive
